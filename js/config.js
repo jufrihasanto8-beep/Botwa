36b94bc8df1b638f5a9654759731490f43a4b21e
jufrihasanto8-beep <jufrihasanto8@gmail.com>
2026-06-08 20:02:08 +0700

    Add files via upload

--- js/config.js
+++ js/config.js
@@ -5,5 +5,5 @@
    2. Settings → API
    3. Copy "Project URL" dan "anon public" key
 */
-window.SUPABASE_URL = 'https://krjcnxibxewviwagavbd.supabase.co';
-window.SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtyamNueGlieGV3dml3YWdhdmJkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkwNzY3MzcsImV4cCI6MjA5NDY1MjczN30.mD4gluev66yyIYjElHlTvNsoc3sulf0oA4PwmoXsMBA';
+window.SUPABASE_URL = 'https://hdplngjvthwcmgkehmvy.supabase.co';
+window.SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhkcGxuZ2p2dGh3Y21na2VobXZ5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA5MTcyMDIsImV4cCI6MjA5NjQ5MzIwMn0.1Q9L9J8txAtcJYq_B3grjpd3NWKskJX2lr5mpCjm9Qg';
